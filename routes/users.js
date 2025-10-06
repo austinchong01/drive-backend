e6e656c73e81190560b1c3f3c22d8a48c9293265
Austin <austin.inhyuk.chong@gmail.com>
2025-10-05 22:08:26 -0700

    Config Changes

--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,14 @@ const validation = require("../middleware/validation");
 const authenticateToken = require("../config/jwt");
 
 router.post("/auth/register", validation.validateUser, user.createUser);
-
 router.post("/auth/login", user.login);
 router.post("/auth/logout", authenticateToken, user.logout);
 
 router.get("/profile", authenticateToken, user.getUser);
 router.get("/storage", authenticateToken, user.getStorage);
+
 router.patch("/profile", authenticateToken, validation.validateNewUsername, user.updateUsername);
+
 router.delete("/profile/", authenticateToken, user.deleteUser);
 
 module.exports = router;
