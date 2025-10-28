64d06c8f45be6941061b16bde6bae1ae41c5e3c5
aweston <austin.inhyuk.chong@gmail.com>
2025-10-28 15:10:00 -0700

    300 ratelimiter

--- middleware/ratelimiter.js
+++ middleware/ratelimiter.js
@@ -14,7 +14,7 @@ const login = rateLimit({
 
 const api = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 minutes
-  max: 100,
+  max: 300, //  change to 100
   standardHeaders: true,
   legacyHeaders: false,
   handler: (req, res, next) => {
